af35ae0a88c1fb605c2f5d3322d1e32dd7fbec1d
Flow <flowtenshi@gmail.com>
2017-03-11 07:18:38 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(76, 479)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(389, 18)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.Nos4DELauncher.My.Resources.Resources.Quit
+        Me.PictureBox1.Location = New System.Drawing.Point(647, 470)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(120, 66)
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Image = Global.Nos4DELauncher.My.Resources.Resources.buttonstart
+        Me.PictureBox2.Location = New System.Drawing.Point(513, 470)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(126, 69)
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox3.Image = Global.Nos4DELauncher.My.Resources.Resources.Homepage
+        Me.PictureBox3.Location = New System.Drawing.Point(162, 369)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(118, 66)
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox4.Image = Global.Nos4DELauncher.My.Resources.Resources.Forum
+        Me.PictureBox4.Location = New System.Drawing.Point(29, 369)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(115, 66)
+        Me.PictureBox4.TabIndex = 9
+        Me.PictureBox4.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(313, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(454, 181)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(313, 254)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(454, 181)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Location = New System.Drawing.Point(703, 229)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Location = New System.Drawing.Point(703, 413)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Label4"
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(76, 509)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(389, 18)
+        Me.ProgressBar2.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Nos4DELauncher.My.Resources.Resources.LauncherN4D
+        Me.ClientSize = New System.Drawing.Size(800, 548)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "NosTale4DE Launcher"
+        Me.TransparencyKey = System.Drawing.SystemColors.Control
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ProgressBar2 As ProgressBar
+End Class
---
+++ Form1.vb
@@ -0,0 +1,190 @@
+﻿Imports System.IO
+Imports MySql
+Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Public Class Form1
+    Public IP As String
+    Public Port As String
+    Public c As New MySqlConnection
+    Public cmd As New MySqlCommand
+    Public da As New MySqlDataAdapter
+    Public dr As MySqlDataReader
+    Public ds, ds1 As New DataSet
+    Public myConnectionString As String
+    Dim X As Integer
+    Dim Y As Integer
+	Public databaseip as String = "127.0.0.1"
+	Public username as String = "root"
+	Public password as String = "root"
+    Public database As String = "Nostale"
+    Public Forum As String = "http://board.nostale.de/"
+    Public Website As String = "http://nostale.de/"
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        X = e.X
+        Y = e.Y
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If e.Button = e.Button.Left Then
+            Me.Left = e.X - X + Me.Left
+            Me.Top = e.Y - Y + Me.Top
+        End If
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+    End Sub
+
+    Private Sub NosTale4DELauncher_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Try
+            myConnectionString = "server=" + databaseip + ";uid=" + username + ";pwd=" + password + ";database=" + database + "; "
+            c.ConnectionString = myConnectionString
+            ds.Clear()
+            c.Open()
+            cmd = New MySqlCommand("select * from launcher", c)
+            da = New MySqlDataAdapter(cmd)
+            da.Fill(ds, "launcher")
+            IP = ds.Tables(0).Rows(0).Item(1)
+            Port = ds.Tables(0).Rows(0).Item(2)
+            ds.Clear()
+            cmd = New MySqlCommand("SELECT title,Content_launcher,date FROM blog SORT ORDER BY id DESC Limit 2", c)
+            da = New MySqlDataAdapter(cmd)
+            da.Fill(ds, "blog")
+            Label1.Text = ds.Tables(1).Rows(0).Item(0)
+            Label1.Text += vbNewLine
+            Label1.Text += vbNewLine
+            Label1.Text += ds.Tables(1).Rows(0).Item(1)
+            Label2.Text = ds.Tables(1).Rows(1).Item(0)
+            Label2.Text += vbNewLine
+            Label2.Text += vbNewLine
+            Label2.Text += ds.Tables(1).Rows(1).Item(1)
+            Label3.Text = ds.Tables(1).Rows(0).Item(2)
+            Label4.Text = ds.Tables(1).Rows(1).Item(2)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        c.Close()
+    End Sub
+
+    Private Sub ProgressBar2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Application.Exit()
+
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        Application.Exit()
+
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseMove
+        PictureBox1.Image = My.Resources.Quit21
+
+    End Sub
+
+    Private Sub PictureBox1_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.MouseLeave
+        PictureBox1.Image = My.Resources.Quit
+    End Sub
+
+    Private Sub PictureBox2_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox2.MouseMove
+        PictureBox2.Image = My.Resources.Start41
+
+    End Sub
+
+    Private Sub PictureBox2_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox2.MouseLeave
+        PictureBox2.Image = My.Resources.buttonstart
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        PictureBox1.Image = My.Resources.Quit
+    End Sub
+
+    Private Sub PictureBox3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox3.Click
+        Process.Start(Website)
+    End Sub
+
+    Private Sub PictureBox4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox4.Click
+        Process.Start(Forum)
+    End Sub
+
+
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Dim Test As String
+        Dim Test2 As String
+        Test = "IP="
+        Test2 = "Port="
+        Dim IP2 As String = (Test + IP)
+        Dim PORT2 As String = (Test2 + Port)
+        If Not File.Exists(Application.StartupPath + "/NostaleX.dat") Then
+            MessageBox.Show("Please move the Launcher into your Nostale Folder")
+        Else
+            Dim path As String = Application.StartupPath + "/config.ini"
+            Dim Checked As Boolean = False
+            Dim port__1 As Boolean = False
+            Dim ip__2 As Boolean = False
+            If File.Exists(path) Then
+
+                Dim list As New List(Of String)
+                Using r As StreamReader = New StreamReader(path)
+                    Dim line As String
+                    line = r.ReadLine
+                    Do While (Not line Is Nothing)
+                        list.Add(line)
+                        Console.WriteLine(line)
+                        line = r.ReadLine
+                    Loop
+                    r.Close()
+                End Using
+                If list.Contains(PORT2) Then
+                    port__1 = True
+                End If
+                If list.Contains(IP2) Then
+                    ip__2 = True
+                End If
+                If ip__2 AndAlso port__1 Then
+                    Checked = True
+                Else
+                    File.Delete(path)
+                    Using sw As StreamWriter = File.CreateText(path)
+                        sw.WriteLine("[NosTale_Network]")
+                        sw.WriteLine(IP2)
+                        sw.WriteLine(PORT2)
+                        Checked = True
+                        sw.Close()
+                    End Using
+                End If
+            Else
+                Using sw As StreamWriter = File.CreateText(path)
+                    sw.WriteLine("[NosTale_Network]")
+                    sw.WriteLine(IP2)
+                    sw.WriteLine(PORT2)
+                    Checked = True
+                    sw.Close()
+                End Using
+            End If
+            If Checked Then
+                Dim nostalex As String = (Application.StartupPath + "/NostaleX.dat")
+                Dim path2 As String = """"
+                path2 += nostalex
+                path2 += """"
+                Dim argument As String = "/c START "
+                argument += """"
+                argument += """"
+                argument += " "
+                argument += path2
+                argument += " EntwellNostaleClientLoadFromIni"
+                Dim p As New Process()
+                p.StartInfo = New ProcessStartInfo("cmd.exe", argument)
+                p.StartInfo.CreateNoWindow = True
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+                p.Start()
+                Application.[Exit]()
+            End If
+        End If
+    End Sub
+End Class
